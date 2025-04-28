2ee8486c6495f780fe0b02fd0ed34555bd68cdd7
juliwonrenta13 <blnp845@gmail.com>
2025-04-28 08:22:19 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+
+// lightbox untuk galeri dan fasilitas
+document.querySelectorAll('.-galeri-container img').forEach(img => {
+  img.addEventListener('click', () => {
+    const overlay = document.createElement('div');
+    overlay.className = 'lightbox-overlay';
+    overlay.innerHTML = `<img src="${img.src}" class="lightbox-img">`;
+    document.body.appendChild(overlay);
+    overlay.addEventListener('click', () => overlay.remove());
+  });
+});
+
+// JS ESKUL
+function showInfo(nama) {
+  alert("Kamu memilih ekstrakurikuler: " + nama);
+}
+
+// Fungsi interaktif untuk bagian prestasi
+function showPrestasi(pesan) {
+  alert("Informasi Prestasi: " + pesan);
+}
+
+// hubungi kami
+function kirimPesan(event) {
+  event.preventDefault();
+  const nama = document.getElementById('nama').value;
+  if (nama.trim() !== "") {
+    alert(`Terima kasih telah menghubungi kami, ${nama}!`);
+    document.getElementById('formHubungi').reset();
+  } else {
+    alert('Mohon isi nama Anda.');
+  }
+}
